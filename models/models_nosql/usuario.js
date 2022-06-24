0039a2fd3c00fbbde03222f5655dedaac5705e6e
Tiago Lima <tiago4contact@gmail.com>
2022-06-24 01:41:31 -0300

    Commit de alterações

--- models/models_nosql/usuario.js
+++ models/models_nosql/usuario.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Usuario = Schema({
+    nome: { type: String, required: true }, 
     login: { type: String, required: true },
     senha: { type: String, required: true },
-    pergunta_secreta: { type: String, required: true },
-    resposta_pergunta: { type: String, required: true }
+    datanasc: { type: Date, required: true },
+    email: { type: String, required: true },
+    tipo: { type: String, required: true }  
 });
 
 module.exports = mongoose.model("Usuario", Usuario);
