8a60b66743f572ca2674d8f7fba001ed9af8542d
Tiago Lima <tiago4contact@gmail.com>
2022-06-25 16:29:20 -0300

    commit

---
+++ controllers/controllerCreche.js
@@ -0,0 +1,34 @@
+const Creche = require('../models/models_nosql/creche');
+
+module.exports = {
+
+    async getCreate(req, res) {
+        res.render('creche/crecheCreate');
+    },
+    async postCreate(req, res) {
+        const {nome, bairro, endereco} = req.body;
+        const creche = new Creche({nome, bairro, endereco});
+        await creche.save();
+        res.redirect('/home');
+    },
+    async getList(req, res) {
+        Creche.find().then((creches) => {
+            res.render('creche/crecheList', {creches: creches.map(creches => creches.toJSON())});
+        });
+    },
+    async getEdit(req, res) {
+        await Creche.findOne({_id:req.params.id}).then((creches)=>{
+        res.render('creche/crecheEdit', {creches:creches.toJSON()});
+    });
+    },
+    async postEdit(req, res) {
+        const {nome, bairro, endereco} = req.body;
+        await Creche.findOneAndUpdate({_id:req.body.id}, {nome, bairro, endereco});
+        res.redirect('/crecheList');
+    },
+    async getDelete(req, res) {
+        await Creche.findOneAndRemove({_id:req.params.id});
+        res.redirect('/crecheList');
+    }
+                
+}
