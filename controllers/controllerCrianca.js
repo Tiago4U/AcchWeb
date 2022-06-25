2fa8555eb60725f5c1a0e2b6eb92684c435cb3ce
Tiago Lima <tiago4contact@gmail.com>
2022-06-25 16:57:07 -0300

    Commit

--- controllers/controllerCrianca.js
+++ controllers/controllerCrianca.js
@@ -1,34 +1,34 @@
-const Creche = require('../models/models_nosql/creche');
+const Criancas = require('../models/models_nosql/crianca');
 
 module.exports = {
 
     async getCreate(req, res) {
-        res.render('creche/crecheCreate');
+        res.render('crianca/criancaCreate');
     },
     async postCreate(req, res) {
-        const {nome, bairro, endereco} = req.body;
-        const creche = new Creche({nome, bairro, endereco});
-        await creche.save();
+        const {nome, rg, nomedopai, endereco, bairro, datanasc, turma} = req.body;
+        const crianca = new Crianca({nome, rg, nomedopai, endereco, bairro, datanasc, turma});
+        await crianca.save();
         res.redirect('/home');
     },
     async getList(req, res) {
-        Creche.find().then((creches) => {
-            res.render('creche/crecheList', {creches: creches.map(creches => creches.toJSON())});
+        Crianca.find().then((criancas) => {
+            res.render('crianca/criancaList', {criancas: criancas.map(criancas => criancas.toJSON())});
         });
     },
     async getEdit(req, res) {
-        await Creche.findOne({_id:req.params.id}).then((creches)=>{
-        res.render('creche/crecheEdit', {creches:creches.toJSON()});
+        await Crianca.findOne({_id:req.params.id}).then((criancas)=>{
+        res.render('crianca/criancaEdit', {criancas:criancas.toJSON()});
     });
     },
     async postEdit(req, res) {
-        const {nome, bairro, endereco} = req.body;
-        await Creche.findOneAndUpdate({_id:req.body.id}, {nome, bairro, endereco});
-        res.redirect('/crecheList');
+        const {nome, rg, nomedopai, endereco, bairro, datanasc, turma} = req.body;
+        await Crianca.findOneAndUpdate({_id:req.body.id}, {nome, rg, nomedopai, endereco, bairro, datanasc, turma});
+        res.redirect('/criancaList');
     },
     async getDelete(req, res) {
-        await Creche.findOneAndRemove({_id:req.params.id});
-        res.redirect('/crecheList');
+        await Crianca.findOneAndRemove({_id:req.params.id});
+        res.redirect('/criancaList');
     }
                 
 }
