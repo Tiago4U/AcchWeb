b7e7610592259bbac5b297f013e5dfe8e41e9f8e
emanuelsoares07 <santindosanjos@gmail.com>
2022-06-25 16:13:28 -0300

    commit

---
+++ models/models_nosql/creche.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Creche = Schema({
+    nome: { type: String, required: true },
+    bairro: { type: String, required: true },
+    endereco: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Creche", Creche)
