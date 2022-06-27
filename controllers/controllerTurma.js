4e3d2641cefa392a8ac660ed6f51a5f40132914e
Tiago Lima <tiago4contact@gmail.com>
2022-06-27 01:01:47 -0300

    Commit

--- controllers/controllerTurma.js
+++ controllers/controllerTurma.js
@@ -17,21 +17,34 @@ module.exports = {
     },
     async getList(req, res) {
         Turma.find().then((turmas) => {
-            console.log(turmas);
             res.render('turma/turmaList', {turmas: turmas.map(turmas => turmas.toJSON())});
         });
         
     },
     async getEdit(req, res) {
+        console.log('passou1');
+        console.log(req.body);
         await Turma.findOne({_id:req.params.id}).then((turmas)=>{
         res.render('turma/turmaEdit', {turmas:turmas.toJSON()});
+        console.log('passou asd');
     });
     },
+    
+    /*
     async postEdit(req, res) {
+        console.log('passou2');
         const {creche, turma, qtalunos, qtdvagas} = req.body;
         await Turma.findOneAndUpdate({_id:req.body.id}, {creche, turma, qtalunos, qtdvagas});
         res.redirect('/turmaList');
     },
+*/
+    async postEdit(req, res) {
+        console.log('passou2');
+        await Turma.findOneAndUpdate({ _id: req.body.id }, req.body);
+        res.redirect('/turmaList');
+    },
+
+
     async getDelete(req, res) {
         await Turma.findOneAndRemove({_id:req.params.id});
         res.redirect('/turmaList');
--- models/models_nosql/turma.js
+++ models/models_nosql/turma.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const Turmas = Schema({
     creche: { type: String, required: true },
     turma: { type: String, required: true },
-    qtalunos: { type: String, required: true },
-    qtdvagas: { type: String, required: true }
+    qtalunos: { type: Number, required: true },
+    qtdvagas: { type: Number, required: true }
 });
 
 module.exports = mongoose.model("Turmas", Turmas)
